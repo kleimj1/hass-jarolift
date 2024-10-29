b132a2bc77e4a83e9d011ce7d8e363a0d03f246f
kleimj1 <jochen.kleimann@gmail.com>
2024-10-29 21:17:24 +0100

    Update cover.py

--- custom_components/jarolift/cover.py
+++ custom_components/jarolift/cover.py
@@ -11,9 +11,11 @@ from homeassistant.components.cover import (
 )
 from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
+
 CONF_COVERS = "covers"
 CONF_GROUP = "group"
 CONF_SERIAL = "serial"
+
 _COVERS_SCHEMA = vol.All(
     cv.ensure_list,
     [
@@ -26,57 +28,63 @@ _COVERS_SCHEMA = vol.All(
         )
     ],
 )
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_COVERS): _COVERS_SCHEMA,
     }
 )
-DEPENDENCIES = ["jarolift"]
+
 _LOGGER = logging.getLogger(__name__)
-def setup_platform(hass, config, add_devices, discovery_info=None):
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Jarolift covers."""
-    covers = []
-    covers_conf = config.get(CONF_COVERS)
-    for cover in covers_conf:
-        covers.append(
-            JaroliftCover(cover[CONF_NAME], cover[CONF_GROUP], cover[CONF_SERIAL], hass)
-        )
-    add_devices(covers)
+    covers = [JaroliftCover(cover[CONF_NAME], cover[CONF_GROUP], cover[CONF_SERIAL], hass) for cover in config[CONF_COVERS]]
+    async_add_entities(covers)
+
 class JaroliftCover(CoverEntity):
-    """Representation a jarolift Cover."""
+    """Representation of a Jarolift Cover."""
+
     def __init__(self, name, group, serial, hass):
-        """Initialize the jarolift device."""
+        """Initialize the Jarolift device."""
         self._name = name
         self._group = group
         self._serial = serial
         self._hass = hass
-        supported_features = 0
-        supported_features |= CoverEntityFeature.SET_TILT_POSITION
-        supported_features |= CoverEntityFeature.OPEN
-        supported_features |= CoverEntityFeature.CLOSE
-        supported_features |= CoverEntityFeature.STOP
+        supported_features = (
+            CoverEntityFeature.SET_TILT_POSITION | 
+            CoverEntityFeature.OPEN |
+            CoverEntityFeature.CLOSE |
+            CoverEntityFeature.STOP
+        )
         self._attr_supported_features = supported_features
         self._attr_device_class = CoverDeviceClass.BLIND
+
     @property
     def serial(self):
         """Return the serial of this cover."""
         return self._serial
+
     @property
     def name(self):
-        """Return the name of the cover if any."""
+        """Return the name of the cover."""
         return self._name
+
     @property
     def group(self):
-        """Return the name of the group if any."""
+        """Return the group of the cover."""
         return self._group
+
     @property
     def should_poll(self):
-        """No polling available in Jarolift cover."""
+        """No polling needed for Jarolift cover."""
         return False
+
     @property
     def is_closed(self):
         """Return true if cover is closed."""
         return None
+
     async def async_close_cover(self, **kwargs):
         """Close the cover."""
         await self._hass.services.async_call(
@@ -84,6 +92,7 @@ class JaroliftCover(CoverEntity):
             "send_command",
             {"group": self._group, "serial": self._serial, "button": "0x8"},
         )
+
     async def async_open_cover(self, **kwargs):
         """Open the cover."""
         await self._hass.services.async_call(
@@ -91,6 +100,7 @@ class JaroliftCover(CoverEntity):
             "send_command",
             {"group": self._group, "serial": self._serial, "button": "0x2"},
         )
+
     async def async_stop_cover(self, **kwargs):
         """Stop the cover."""
         await self._hass.services.async_call(
@@ -98,8 +108,9 @@ class JaroliftCover(CoverEntity):
             "send_command",
             {"group": self._group, "serial": self._serial, "button": "0x4"},
         )
+
     async def async_set_cover_tilt_position(self, **kwargs):
-        """Drive the cover to tilt position"""
+        """Set the cover tilt position."""
         await self._hass.services.async_call(
             "jarolift",
             "send_command",
